68652791026eccd219f239460ce33478d5038791
Lamis Armoush <lamis.armoush@hotmail.com>
2022-05-03 15:14:33 +0300

    added lematizer just so I won't forget

--- normalizeText.py
+++ normalizeText.py
@@ -7,7 +7,7 @@ nltk.download()
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-
+from nltk.stem import WordNetLemmatizer
 
 def normalizeText(text):
 
@@ -22,7 +22,10 @@ def normalizeText(text):
     # filter out stop words
     stop_words = set(stopwords.words("english"))
     words = [w for w in words if not w in stop_words]
+    #lemantize
+    lematizer = WordNetLemmatizer()
+    lem = [lematizer.lemmatize(word) for word in words]
     # stem words
     porter = PorterStemmer()
-    stemmed = [porter.stem(word) for word in words]
+    stemmed = [porter.stem(word) for word in lem]
     return stemmed
