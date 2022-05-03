d12186fc31ded597d169a25e5649a1937e94e1af
Lamis Armoush <lamis.armoush@hotmail.com>
2022-05-03 21:25:19 +0300

    indexing adjusted

--- functions/indexing.py
+++ functions/indexing.py
@@ -1,20 +1,20 @@
-from preprocess import getAllTerms
+from Vectors import getAllVectors
 from pprint import pprint
 
-def getIndexingTable(list,XMLpaths):
-    dic={"":()}
-    i =0
-    for arr in list:
-        for tuple in arr:
-            term = tuple[0]
-            if term in dic:
-                if XMLpaths[i] not in dic[term] :
-                    dic[term]=dic[term]+ (XMLpaths[i],)
+def getIndexingTable(arrDics,XMLpaths):
+    out={}
+    i = 0
+    for dic in arrDics:
+        for key in dic:
+            term = key [0] 
+            if (term in out.keys()):
+                if(XMLpaths[i] not in out[term]):
+                    out[term]= out[term] + (XMLpaths[i],)    
             else:
-                dic[term] = (XMLpaths[i],)
+                out[term] = (XMLpaths[i],)
         i +=1;
         
-    return dic
+    return out
 
 XMLpaths = [
     "XMLdocuments/doc1.xml",
@@ -22,7 +22,7 @@ XMLpaths = [
     "XMLdocuments/doc3.xml",
     "XMLdocuments/doc4.xml",
 ]
-list = getAllTerms(XMLpaths)
+list = getAllVectors(XMLpaths)
 dic =  getIndexingTable(list,XMLpaths)
 pprint(dic)
 
